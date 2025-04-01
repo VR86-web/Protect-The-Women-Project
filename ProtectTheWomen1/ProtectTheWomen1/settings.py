125cb7ad3feb80facd483e2474a9bdd6cbf08632
VR86-web <145848782+VR86-web@users.noreply.github.com>
2025-04-01 19:19:50 +0200

    feat: added some setups

--- ProtectTheWomen1/ProtectTheWomen1/settings.py
+++ ProtectTheWomen1/ProtectTheWomen1/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 SESSION_COOKIE_HTTPONLY = True
 
+CSRF_COOKIE_SECURE = True 
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
